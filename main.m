a8b3530d54e0cf8abf167895a1984c586c44da49
hwjwill <hwjwill@hotmail>
2016-09-28 02:19:13 -0700

    Half finished with part 1

--- main.m
+++ main.m
@@ -14,34 +14,40 @@ end
 %% Part 1: Hybrid Images
 if part == 1
     % read images and convert to single format
-    im1 = im2single(imread('./DerekPicture.jpg'));
-    im2 = im2single(imread('./nutmeg.jpg'));
-    im1 = rgb2gray(im1); % convert to grayscale
-    im2 = rgb2gray(im2);
+    im1 = im2single(imread('./cat.jpg'));
+    im2 = im2single(imread('./tiger.jpg'));
+%    im1 = rgb2gray(im1); % convert to grayscale
+%    im2 = rgb2gray(im2);
     
     % use this if you want to align the two images (e.g., by the eyes) and crop
     % them to be of same size
     [im2, im1] = align_images(im2, im1);
-% uncomment this when debugging hybridImage so that you don't have to keep aligning
-% keyboard; 
+    % uncomment this when debugging hybridImage so that you don't have to keep aligning
+    % keyboard; 
 
-%% Choose the cutoff frequencies and compute the hybrid image (you supply
-%% this code)
-arbitrary_value = 100;
-cutoff_low = arbitrary_value;
-cutoff_high = arbitrary_value; 
-im12 = hybridImage(im1, im2, cutoff_low, cutoff_high);
+    cutoff_low = 5;
+    cutoff_high = 3; 
+    im12 = hybridImage(im1, im2, cutoff_low, cutoff_high);
+    imshow(im12);
 
-%% Crop resulting image (optional)
-figure(1), hold off, imagesc(im12), axis image, colormap gray
-disp('input crop points');
-[x, y] = ginput(2);  x = round(x); y = round(y);
-im12 = im12(min(y):max(y), min(x):max(x), :);
-figure(1), hold off, imagesc(im12), axis image, colormap gray
+%     %% Crop resulting image (optional)
+%     figure(1), hold off, imagesc(im12), axis image, colormap gray
+%     disp('input crop points');
+%     [x, y] = ginput(2);  x = round(x); y = round(y);
+%     im12 = im12(min(y):max(y), min(x):max(x), :);
+%     figure(1), hold off, imagesc(im12), axis image, colormap gray
 
-%% Compute and display Gaussian and Laplacian Pyramids (you need to supply this function)
-N = 5; % number of pyramid levels (you may use more or fewer, as needed)
-pyramids(im12, N);
+    % %% Compute and display Gaussian and Laplacian Pyramids (you need to supply this function)
+    % N = 5; % number of pyramid levels (you may use more or fewer, as needed)
+    % pyramids(im12, N);
 
 end
+end
+
+%% hybrid Image implementation
+function [result] = hybridImage(im1, im2, cutoff_low, cutoff_high)
+lowPassed = imgaussfilt(im1, cutoff_low);
+highPassed = im2 - imgaussfilt(im2, cutoff_high);
+
+result = (lowPassed + highPassed) ./ 2;
 end
